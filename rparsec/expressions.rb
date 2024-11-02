185b16f4bb3e5ae88a3ca07b8c8a33a9326e79ae
gemmaro <gemmaro.dev@gmail.com>
2024-11-02 21:36:24 +0900

    Make class methods private.

--- rparsec/expressions.rb
+++ rparsec/expressions.rb
@@ -98,16 +98,18 @@ end
 # that parses the term expression.
 #  
 module Expressions
-  private
-  
-  def self.array_to_dict arr
-    result = {}
-    arr.each_with_index do |key,i|
-      result [key] = i unless result.include? key
+  class << self
+    private
+
+    def array_to_dict arr
+      result = {}
+      arr.each_with_index do |key,i|
+        result [key] = i unless result.include? key
+      end
+      result
     end
-    result
   end
-  
+
   KindPrecedence = array_to_dict Associativities
   
   public
@@ -122,27 +124,28 @@ module Expressions
     # sort so that higher precedence first.
     apply_operators(term, prepare_suites(table).sort, delim)
   end
-  
-  private
-  
-  def self.apply_operators(term, entries, delim)
-  # apply operators stored in [[precedence,associativity],[op...]] starting from beginning.
-    entries.inject(term) do |result, entry|
-      key, ops = *entry
-      _, kind_index = *key
-      op = ops[0]
-      op = Parsers.sum(*ops) if ops.length>1
-      apply_operator(result, op, Associativities[kind_index], delim)
+
+  class << self
+    private
+
+    def apply_operators(term, entries, delim)
+      # apply operators stored in [[precedence,associativity],[op...]] starting from beginning.
+      entries.inject(term) do |result, entry|
+        key, ops = *entry
+        _, kind_index = *key
+        op = ops[0]
+        op = Parsers.sum(*ops) if ops.length>1
+        apply_operator(result, op, Associativities[kind_index], delim)
+      end
     end
-  end
-  
-  def self.apply_operator(term, op, kind, delim)
-    term, op = ignore_rest(term, delim), ignore_rest(op, delim)
-    # we could use send here, 
-    # but explicit case stmt is more straight forward and less coupled with names.
-    # performance might be another benefit,
-    # though it is not clear whether meta-code is indeed slower than regular ones at all.
-    case kind
+
+    def apply_operator(term, op, kind, delim)
+      term, op = ignore_rest(term, delim), ignore_rest(op, delim)
+      # we could use send here,
+      # but explicit case stmt is more straight forward and less coupled with names.
+      # performance might be another benefit,
+      # though it is not clear whether meta-code is indeed slower than regular ones at all.
+      case kind
       when :prefix
         term.prefix(op)
       when :postfix
@@ -155,29 +158,30 @@ module Expressions
         term.infixn(op)
       else
         raise ArgumentError, "unknown associativity: #{kind}"
-    end
-  end
-  
-  def self.ignore_rest(parser, delim)
-    return parser if delim.nil?
-    parser << delim
-  end
-  
-  def self.prepare_suites(table)
-  # create a hash with [precedence, associativity] as key, and op as value.
-    suites = {}
-    table.operators.each do |entry|
-      kind, op, precedence = *entry
-      key = [-precedence, KindPrecedence[kind]]
-      suite = suites[key]
-      if suite.nil?
-        suite = [op]
-        suites[key] = suite
-      else
-        suite << op
       end
     end
-    suites
+
+    def ignore_rest(parser, delim)
+      return parser if delim.nil?
+      parser << delim
+    end
+
+    def prepare_suites(table)
+      # create a hash with [precedence, associativity] as key, and op as value.
+      suites = {}
+      table.operators.each do |entry|
+        kind, op, precedence = *entry
+        key = [-precedence, KindPrecedence[kind]]
+        suite = suites[key]
+        if suite.nil?
+          suite = [op]
+          suites[key] = suite
+        else
+          suite << op
+        end
+      end
+      suites
+    end
   end
 end
 
