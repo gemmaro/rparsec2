6fd3c9fe4f4ad0bd062a43a501d0252a10b0794b
gemmaro <gemmaro.dev@gmail.com>
2024-11-02 23:41:32 +0900

    Rewrite module eval with block instead of multiline string.

--- lib/rparsec/misc.rb
+++ lib/rparsec/misc.rb
@@ -119,11 +119,11 @@ module Signature
       TypeChecker.check_vararg_type star_type, params, sym, star_ind unless star_ind.nil?
       mtd.bind(self)
     end
-    module_eval "
-    def #{sym}(*params, &block)
-      #{helper}(*params).call(*params, &block)
+    module_eval do
+      define_method(sym) do |*params, &block|
+        send(helper, *params).call(*params, &block)
+      end
     end
-    "
   end
 end
 
