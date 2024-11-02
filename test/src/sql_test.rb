faf59d88db699a5c4835903b8d77bcc9047526bf
gemmaro <gemmaro.dev@gmail.com>
2024-11-02 21:55:30 +0900

    Remove trailing whitespace.

--- test/src/sql_test.rb
+++ test/src/sql_test.rb
@@ -16,7 +16,7 @@ class SqlTestCase < ParserTestCase
     verify_sql('1+2+3', '((1 + 2) + 3)', expression)
   end
   def testExpressionWithBool
-    verify_sql('1+Case 2 when 1 then x else dbo.y end', 
+    verify_sql('1+Case 2 when 1 then x else dbo.y end',
       '(1 + case 2 when 1 then x else dbo.y end)', expression)
   end
   def testExpressionWithWildcard
@@ -36,50 +36,50 @@ class SqlTestCase < ParserTestCase
     verify_relation("select $a,* from [table 1]", "select $a, * from table 1")
   end
   def testSubRelation
-    verify_relation('select * from (select a, b, c.* from c)', 
+    verify_relation('select * from (select a, b, c.* from c)',
       'select * from (select a, b, c.* from c)')
   end
   def testSimpleRelationWithAlias
     verify_relation('select x.* from table1 x', 'select x.* from table1 AS x')
   end
   def testSubRelationWithAlias
-    verify_relation('select * from ((select a, b, c.* from c)) x', 
+    verify_relation('select * from ((select a, b, c.* from c)) x',
       'select * from (select a, b, c.* from c) AS x')
   end
   def testRelationWithWhere
-    verify_relation('select * from table where x=1', 
+    verify_relation('select * from table where x=1',
       'select * from table where x = 1')
   end
   def testRelationWithWhereAndDistinct
-    verify_relation('select distinct * from table where x=1', 
+    verify_relation('select distinct * from table where x=1',
       'select distinct * from table where x = 1')
   end
   def testRelationWithCompoundPredicateInWhereClause
-    verify_relation('select * from table where x=1 and y=3', 
+    verify_relation('select * from table where x=1 and y=3',
       'select * from table where (x = 1 and y = 3)')
   end
   def testRelationWithOrderBy
-    verify_relation('select distinct * from table where x=1 order by x asc, y desc', 
+    verify_relation('select distinct * from table where x=1 order by x asc, y desc',
       'select distinct * from table where x = 1 order by x, y desc')
   end
   def testRelationWithOrderByAndLimit
-    verify_relation('select distinct * from table where x=1 order by x asc, y desc limit 5', 
+    verify_relation('select distinct * from table where x=1 order by x asc, y desc limit 5',
       'select distinct * from table where x = 1 order by x, y desc limit 5')
   end
   def testRelationWithGroupByWithoutHaving
-    verify_relation('select distinct * from table where x=1 group by x, y order by x asc, y desc', 
+    verify_relation('select distinct * from table where x=1 group by x, y order by x asc, y desc',
       'select distinct * from table where x = 1 group by x, y order by x, y desc')
   end
   def testRelationWithGroupByWithHaving
-    verify_relation('select distinct * from table where x=1 group by x, y having x!=y order by x asc, y desc', 
+    verify_relation('select distinct * from table where x=1 group by x, y having x!=y order by x asc, y desc',
       'select distinct * from table where x = 1 group by x, y having x <> y order by x, y desc')
   end
   def testRelationWithSimpleJoin
-    verify_relation('select * from table1 t1 inner join table2 t2 on t1.a=t2.b where x=1', 
+    verify_relation('select * from table1 t1 inner join table2 t2 on t1.a=t2.b where x=1',
       'select * from table1 AS t1 inner join table2 AS t2 on t1.a = t2.b where x = 1')
   end
   def testRelationWithMultiJoins
-    verify_relation('select * from table1 t1 inner join table2 t2 right outer join table3 on t2.x>t3.y on t1.a=t2.b cross join table3 where x=1', 
+    verify_relation('select * from table1 t1 inner join table2 t2 right outer join table3 on t2.x>t3.y on t1.a=t2.b cross join table3 where x=1',
       'select * from table1 AS t1 inner join table2 AS t2 right join table3 on t2.x > t3.y on t1.a = t2.b cross join table3 where x = 1')
   end
   def testRelationWithExists
