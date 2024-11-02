1b146c237f2570a9a275e8e45062216beea1a930
gemmaro <gemmaro.dev@gmail.com>
2024-11-02 21:58:28 +0900

    Lint spacing around operators.

--- rparsec/locator.rb
+++ rparsec/locator.rb
@@ -12,13 +12,13 @@ class CodeLocator
   def locate(ind)
     return _locateEof if ind >= code.length
     line, col = 1, 1
-    return line, col if ind<=0
+    return line, col if ind <= 0
     for i in (0...ind)
       c = code[i]
       if c == LF
-        line, col = line+1, 1
+        line, col = line + 1, 1
       else
-        col = col+1
+        col = col + 1
       end
     end
     return line, col
@@ -28,9 +28,9 @@ class CodeLocator
     line, col = 1, 1
     code.each_byte do |c|
       if c == LF.ord
-        line, col = line+1, 1
+        line, col = line + 1, 1
       else
-        col = col+1
+        col = col + 1
       end
     end
     return line, col
