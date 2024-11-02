d7b35e1aba6351ff9837d07263f26a3bc2468808
gemmaro <gemmaro.dev@gmail.com>
2024-11-02 21:54:26 +0900

    Remove trailing whitespace.

--- rparsec/monad.rb
+++ rparsec/monad.rb
@@ -1,11 +1,11 @@
 module RParsec
-  
+
 #
 # module for Monad
 #
 module Monad
   attr_reader :this
-  
+
   #
   # To initialize with a monad implementation and an object that obeys the monad law.
   #
@@ -14,25 +14,25 @@ module Monad
     @monad = m;
     @this = v;
   end
-  
+
   #
   # To create a value based on the monad impl.
-  # 
+  #
   def value v
     @monad.value v
   end
-  
+
   #
   # Run the _bind_ operation on the encapsulated object following the monad law.
-  # 
+  #
   def bind(&binder)
     @monad.bind(@this, &binder)
   end
-  
+
   #
   # Run the _seq_ operation on the encapsulated object following the monad law.
   # If _seq_ is not defined by the monad impl, use _bind_ to implement.
-  # 
+  #
   def seq(other)
     if @monad.respond_to? :seq
       @monad.seq(other)
@@ -40,21 +40,21 @@ module Monad
       bind {|_x|other}
     end
   end
-  
+
   #
   # Run the _map_ operation on the encapsulated object following the monad law.
   # _bind_ is used to implement.
-  # 
+  #
   def map(&mapper)
     bind do |v|
       result = mapper.call v;
       value(result);
     end
   end
-  
+
   #
   # Run the _plus_ operation on the encapsulated object following the MonadPlus law.
-  # 
+  #
   def plus other
     @monad.mplus(@this, other.this)
   end
