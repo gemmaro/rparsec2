1ae0931dfc3977e65c250c0cf4942d63906d71f9
gemmaro <gemmaro.dev@gmail.com>
2024-11-03 16:27:32 +0900

    Add coverage directory to Emacs directory locals.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (add-to-list 'grep-find-ignored-directories "html")))))
+((nil . ((eval . (add-to-list 'grep-find-ignored-directories "html"))
+         (eval . (add-to-list 'grep-find-ignored-directories "coverage"))))))))
