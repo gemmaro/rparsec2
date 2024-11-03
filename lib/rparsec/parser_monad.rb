e12f52793c2842bd7791228c935cfc8bbbe0282f
gemmaro <gemmaro.dev@gmail.com>
2024-11-03 15:45:09 +0900

    Lint indent width.

--- lib/rparsec/parser_monad.rb
+++ lib/rparsec/parser_monad.rb
@@ -2,24 +2,24 @@
 
 module RParsec
 
-class ParserMonad
-  def fail msg
-    FailureParser.new(msg)
-  end
+  class ParserMonad
+    def fail msg
+      FailureParser.new(msg)
+    end
 
-  def value v
-    return Nil if v.nil?
-    ValueParser.new(v);
-  end
+    def value v
+      return Nil if v.nil?
+      ValueParser.new(v);
+    end
 
-  def bind(v, &proc)
-    return v unless proc
-    BoundParser.new(v, proc);
-  end
+    def bind(v, &proc)
+      return v unless proc
+      BoundParser.new(v, proc);
+    end
 
-  def mplus(p1, p2)
-    PlusParser.new([p1, p2]);
+    def mplus(p1, p2)
+      PlusParser.new([p1, p2]);
+    end
   end
-end
 
 end # module
