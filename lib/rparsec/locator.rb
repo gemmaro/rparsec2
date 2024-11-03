c69e5d22e0426413cf6a03354dfe97c756c536cf
gemmaro <gemmaro.dev@gmail.com>
2024-11-03 15:45:23 +0900

    Lint indent width.

--- lib/rparsec/locator.rb
+++ lib/rparsec/locator.rb
@@ -4,39 +4,39 @@ require 'rparsec/misc'
 
 module RParsec
 
-class CodeLocator
-  extend DefHelper
+  class CodeLocator
+    extend DefHelper
 
-  def_readable :code
+    def_readable :code
 
-  LF = ?\n
+    LF = ?\n
 
-  def locate(ind)
-    return _locateEof if ind >= code.length
-    line, col = 1, 1
-    return line, col if ind <= 0
-    for i in (0...ind)
-      c = code[i]
-      if c == LF
-        line, col = line + 1, 1
-      else
-        col = col + 1
+    def locate(ind)
+      return _locateEof if ind >= code.length
+      line, col = 1, 1
+      return line, col if ind <= 0
+      for i in (0...ind)
+        c = code[i]
+        if c == LF
+          line, col = line + 1, 1
+        else
+          col = col + 1
+        end
       end
+      return line, col
     end
-    return line, col
-  end
 
-  def _locateEof
-    line, col = 1, 1
-    code.each_byte do |c|
-      if c == LF.ord
-        line, col = line + 1, 1
-      else
-        col = col + 1
+    def _locateEof
+      line, col = 1, 1
+      code.each_byte do |c|
+        if c == LF.ord
+          line, col = line + 1, 1
+        else
+          col = col + 1
+        end
       end
+      return line, col
     end
-    return line, col
   end
-end
 
 end # module
