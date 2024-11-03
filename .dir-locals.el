a94e84c140f85dd8a347238eea6ee877a391c20c
gemmaro <gemmaro.dev@gmail.com>
2024-11-04 07:55:29 +0900

    Update Emacs directory locals.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (add-to-list 'grep-find-ignored-directories "html"))
-         (eval . (add-to-list 'grep-find-ignored-directories "coverage"))))))))
+((nil
+  . ((eval
+      . (progn
+          (require 'grep)
+          (add-to-list 'grep-find-ignored-directories "html")
+          (add-to-list 'grep-find-ignored-directories "coverage"))))))
