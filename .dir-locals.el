47209227a1c4fbb1dccd39076795880983293b35
gemmaro <gemmaro.dev@gmail.com>
2024-11-03 15:15:37 +0900

    Add Emacs directory locals file to ignore HTML directory.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (add-to-list 'grep-find-ignored-directories "html")))))
