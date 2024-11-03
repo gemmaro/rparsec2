6828b9ca5f5aa02324160a0db4a0c645973d9124
gemmaro <gemmaro.dev@gmail.com>
2024-11-03 15:45:02 +0900

    Lint indent width.

--- lib/rparsec/token.rb
+++ lib/rparsec/token.rb
@@ -4,42 +4,42 @@ require 'rparsec/misc'
 
 module RParsec
 
-#
-# Represents a token during lexical analysis.
-#
-class Token
-  extend DefHelper
+  #
+  # Represents a token during lexical analysis.
+  #
+  class Token
+    extend DefHelper
 
-  def_ctor :kind, :text, :index
+    def_ctor :kind, :text, :index
 
-  #
-  # The type of the token
-  #
-  attr_reader :kind
+    #
+    # The type of the token
+    #
+    attr_reader :kind
 
-  #
-  # The text of the matched range
-  #
-  attr_reader :text
+    #
+    # The text of the matched range
+    #
+    attr_reader :text
 
-  #
-  # The starting index of the matched range
-  #
-  attr_reader :index
+    #
+    # The starting index of the matched range
+    #
+    attr_reader :index
 
-  #
-  # The length of the token.
-  #
-  def length
-    @text.length
+    #
+    # The length of the token.
+    #
+    def length
+      @text.length
+    end
+
+    #
+    # String representation of the token.
+    #
+    def to_s
+      "#{@kind}: #{@text}"
+    end
   end
-
-  #
-  # String representation of the token.
-  #
-  def to_s
-    "#{@kind}: #{@text}"
-  end
-end
 
 end # module
