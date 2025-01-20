bb793a9153440a1580ba2f405b36a11c03b00c9c
gemmaro <gemmaro.dev@gmail.com>
2025-01-20 21:03:15 +0900

    Remove define constructor method.

--- lib/rparsec/misc.rb
+++ lib/rparsec/misc.rb
@@ -6,17 +6,14 @@ module RParsec
   # Helpers for defining ctor.
   #
   module DefHelper # :nodoc:
-    def def_ctor(*vars)
+    def def_readable(*vars)
+      attr_reader(*vars)
+
       define_method(:initialize) do |*params|
         vars.zip(params) do |var, param|
           instance_variable_set("@#{var}", param)
         end
       end
-    end
-
-    def def_readable(*vars)
-      attr_reader(*vars)
-      def_ctor(*vars)
     end
   end
 
