e332974e7269ca9eff17294f0ee2e4aa6d56889d
gemmaro <gemmaro.dev@gmail.com>
2025-01-20 21:21:23 +0900

    Refactor operator table class.

--- lib/rparsec/operator_table.rb
+++ lib/rparsec/operator_table.rb
@@ -1,8 +1,25 @@
 module RParsec
   #
   # This class holds information about operator precedences and
-  # associativities.  #prefix, #postfix, #infixl, #infixr, #infixn can
-  # be called to register operators.
+  # associativities.  +prefix+, +postfix+, +infixl+, +infixr+, +infixn+ can be
+  # called to register operators.
+  #
+  # +prefix+ :: Defines a prefix operator that returns a unary +Proc+ object
+  #             with a precedence associated.  Returns +self+.
+  #
+  # +postfix+ :: Defines a postfix operator that returns a unary +Proc+ object
+  #              with a precedence associated.  Returns +self+.
+  #
+  # +infixl+ :: Defines a left associative infix operator that returns a
+  #             binary +Proc+ object with a precedence associated. Returns
+  #             +self+.
+  #
+  # +infixr+ :: Defines a right associative infix operator that returns a
+  #             binary +Proc+ object with a precedence associated. Returns
+  #             +self+.
+  #
+  # +infixn+ :: Defines a non-associative infix operator that returns a binary
+  #             +Proc+ object with a precedence associated. Returns +self+.
   #
   class OperatorTable
 
@@ -12,15 +29,8 @@ module RParsec
     attr_reader :operators # :nodoc:
 
     #
-    # Re-initialize the operator table.
-    #
-    def reinit # :nodoc:
-      @operators = []
-    end
-
-    #
-    # To create an OperatorTable instance.  If a block is given, it is
-    # invoked to do post-instantiation.  For example:
+    # To create an OperatorTable instance.  If a block is given, it is invoked
+    # to do post-instantiation.  For example:
     #
     #   OperatorTable.new do |tbl|
     #     tbl.infixl(char(?+) >> Plus, 10)
@@ -30,60 +40,16 @@ module RParsec
     #     tbl.prefix(char(?-) >> Neg, 50)
     #   end
     #
-    def self.new
-      this = allocate
-      this.reinit
-      if block_given?
-        yield this
+    def initialize
+      @operators = []
+      block_given? and yield self
+    end
+
+    %i[prefix postfix infixl infixr infixn].each do |method|
+      define_method(method) do |op, precedence|
+        @operators << [method, op, precedence]
+        self
       end
-      this
-    end
-
-    #
-    # Defines a prefix operator that returns a unary +Proc+ object with a precedence associated.
-    # Returns +self+.
-    #
-    def prefix(op, precedence)
-      add(:prefix, op, precedence)
-    end
-
-    #
-    # Defines a postfix operator that returns a unary +Proc+ object with a precedence associated.
-    # Returns +self+.
-    #
-    def postfix(op, precedence)
-      add(:postfix, op, precedence)
-    end
-
-    #
-    # Defines a left associative infix operator that returns a binary +Proc+ object with a precedence
-    # associated. Returns +self+.
-    #
-    def infixl(op, precedence)
-      add(:infixl, op, precedence)
-    end
-
-    #
-    # Defines a right associative infix operator that returns a binary +Proc+ object with a precedence
-    # associated. Returns +self+.
-    #
-    def infixr(op, precedence)
-      add(:infixr, op, precedence)
-    end
-
-    #
-    # Defines a non-associative infix operator that returns a binary +Proc+ object with a precedence
-    # associated. Returns +self+.
-    #
-    def infixn(op, precedence)
-      add(:infixn, op, precedence)
-    end
-
-    private
-
-    def add(*entry)
-      @operators << entry
-      self
     end
   end
 end
